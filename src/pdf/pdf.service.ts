bd1f79ebd6cb7c731fe54f89a537230e175ba9e5
Shalani Kuruppu <shalanikuruppu08@gmail.com>
2025-05-15 11:41:22 +0530

    fix: format import statements for consistency in pdf.service.ts

--- src/pdf/pdf.service.ts
+++ src/pdf/pdf.service.ts
@@ -3,7 +3,7 @@ import * as puppeteer from 'puppeteer';
 import * as path from 'path';
 import * as fs from 'fs';
 import * as Handlebars from 'handlebars';
-import { generateLineChart, generatePieChart ,generateGroupedBarChart} from './chart.util';
+import { generateLineChart, generatePieChart, generateGroupedBarChart } from './chart.util';
 
 
 @Injectable()
