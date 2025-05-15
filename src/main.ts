4c03e0780a9a2278025e0dc51a1f734b4f66162b
Shalani Kuruppu <shalanikuruppu08@gmail.com>
2025-05-15 14:47:38 +0530

    refactor: remove unused AppController and AppService, update main.ts to use PdfModule

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { PdfModule } from './pdf/pdf.module';
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(PdfModule);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
